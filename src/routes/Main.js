744b5b4d9533ecdcc6c987c04ee8b9e8164922c3
cwonseo <dnjstj88@naver.com>
2023-08-23 15:37:22 +0900

    test cicd

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -3,7 +3,7 @@ function Main() {
   return (
     <div>
       <Nav />
-      <div style={{fontSize:"100px",marginTop:"50px"}}> 메인페이지입니다~!!</div>
+      <div style={{fontSize:"100px",marginTop:"50px"}}> 메인페이지입니다~!! cicd 테스트용</div>
     </div>
   );
 }
