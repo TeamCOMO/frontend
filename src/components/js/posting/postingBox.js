96bdac03df496eeb3f2d163bea448374dca6f93b
Cwonseo <dnjstj88@naver.com>
2023-09-11 18:51:18 +0900

    E

--- src/components/js/posting/postingBox.js
+++ src/components/js/posting/postingBox.js
@@ -7,6 +7,7 @@ function PostingBox(e) {
       <div>제목: {e.param.title}</div>
       <div>본문: {e.param.body}</div>
       <div>카테고리: {e.param.category}</div>
+      <div>기술 스택: {e.param.techs}</div>
     </div>
   );
 }
