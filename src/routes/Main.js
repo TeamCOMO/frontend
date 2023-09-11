d4aaa56c12addd62d906597759624e732e571942
kimjuyoung99 <jjassb404@naver.com>
2023-09-11 15:41:32 +0900

    [add]0911주영이

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -4,11 +4,23 @@ import styles from "../components/css/Maincss.module.css";
 function Main() {
   return (
     <div>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+        rel="stylesheet"
+      />
       <Nav />
-      <div className={styles.background_image}></div>
-      <div className="main_introduce">
-        <h1>welcome to COMO</h1>
-        <span>This is some text.</span>
+      <div className={styles.background_image}>
+        {" "}
+        <h1 class="animate__animated animate__fadeInLeftBig">
+          <div className={styles.explain}>
+            <h1>welcome to COMO</h1>
+            <span>This is some text.</span>
+          </div>
+        </h1>
       </div>
     </div>
   );
