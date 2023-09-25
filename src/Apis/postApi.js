a0b551b5a8d813400bd0c23a2b3659e31c6f4206
최원서 <cwonseo@choewonseoui-MacBookAir.local>
2023-09-25 16:45:08 +0900

    FEAT: 게시물 단건조회

--- src/Apis/postApi.js
+++ src/Apis/postApi.js
@@ -30,10 +30,12 @@ export const writePostApi = (postInfo, token, tech) => {
   );
 };
 
-export const getPostApi = (postId,token) =>{
-  return axios.get(
-    `${API}/api/v1/post/${postId}`,{headers:{
-       Authorization: token,
-      "Content-Type": "application/json"}}
-  )
-}
+export const getPostApi = (postId, token) => {
+  return axios.get(`${API}/api/v1/post/${postId}`, {
+    headers: {
+      Authorization: token,
+
+      "Content-Type": "application/json",
+    },
+  });
+};
--- src/routes/PostDetail.js
+++ src/routes/PostDetail.js
@@ -1,49 +1,47 @@
-import { useParams } from "react-router-dom"
-import { getPostApi } from "../Apis/postApi"
-import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom";
+import { getPostApi } from "../Apis/postApi";
+import { useEffect, useState } from "react";
 import postStyle from "./postStyle.module.css";
+import Nav from "../components/js/Nav";
+function PostDetail() {
+  const [postInfo, setPostInfo] = useState({});
+  const token = localStorage.accessToken;
+  const postingId = useParams().postId;
+  console.log(postInfo);
+  useEffect(() => {
+    getPostApi(postingId, token)
+      .then((res) => {
+        setPostInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-import Nav from "../components/js/Nav"
-function PostDetail(){
-    const [postInfo,setPostInfo] = useState({})
-    const token = localStorage.accessToken
-    const postingId = (useParams().postId)
-console.log(postInfo)
-    useEffect(()=>  
-    {
-        getPostApi(postingId,token).then(
-        (res)=>{
-          setPostInfo(res.data)}
-      ).catch((err)=>{console.log(err)})},
-      [])
-  
-    return(<div>
-              <Nav />
-              <div className={postStyle.totalPostingBox}>
+  return (
+    <div>
+      <Nav />
+      <div className={postStyle.totalPostingBox}>
+        <div>
+          <span>제목 : </span>
+          <span>{postInfo.title}</span>
+        </div>
+        <div>
+          <span>내용 : </span>
+          <span>{postInfo.body}</span>
+        </div>
 
         <div>
-            <span>제목 : </span>
-            <span>{postInfo.title}</span>
-            </div>
-            <div>
-            <span>내용 : </span>
-            <span>{postInfo.body}</span>
-            </div>
+          <span>카테고리 : </span>
+          <span>{postInfo.category}</span>
+        </div>
 
-            <div>
-            <span>카테고리 : </span>
-            <span>{postInfo.category}</span>
-            </div>
-
-            <div>
-            <span>기술스택 : </span>
-            <span>{postInfo.techs}</span>
-            </div>
-
-
-        
-            </div>
-        
-        </div>)
+        <div>
+          <span>기술스택 : </span>
+          <span>{postInfo.techs}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default PostDetail
+export default PostDetail;
