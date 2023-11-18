b6d83f7fa279322c5d2919b656ed19520bb6f075
최원서 <dnjstj88@naver.com>
2023-11-18 12:24:30 +0900

    feat:글 상세조회, 전체조회 css 구현

--- src/components/js/Apply/ApplyBtn.js
+++ src/components/js/Apply/ApplyBtn.js
@@ -3,7 +3,10 @@ import { applyApi } from '../../../Apis/postApi';
 function ApplyBtn(props) {
   const apply = () => {
     applyApi(props.postId)
-      .then((res) => {})
+      .then((res) => {
+        console.log(res);
+        alert('신청되었습니다!');
+      })
       .catch((err) => console.log(err));
   };
   return <button onClick={apply}>신청버튼</button>;
--- src/routes/PostDetail.js
+++ src/routes/PostDetail.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import postStyle from './postStyle.module.css';
 import Nav from '../components/js/Nav';
 import ApplyBtn from '../components/js/Apply/ApplyBtn';
+import styled from '@emotion/styled';
+import { WritingDate } from '../components/js/posting/postingBox';
 
 function PostDetail() {
   const [postInfo, setPostInfo] = useState({});
@@ -32,35 +34,95 @@ function PostDetail() {
   return (
     <div>
       <Nav />
-      <div className={postStyle.totalPostingBox}>
-        <div>
-          <img style={{ width: '100px' }} src={postInfo.images}></img>
-        </div>
-        <div>
-          <span>제목 : </span>
-          <span>{postInfo.title}</span>
-        </div>
-        <div>
-          <span>내용 : </span>
-          <span>{postInfo.body}</span>
-        </div>
+      <Background className={postStyle.background}>
+        <div className={postStyle.totalPostingBox}>
+          <PostContatiner>
+            <PostDetailContainer>
+              <div>
+                <img style={{ width: '100px' }} src={postInfo.images}></img>
+              </div>
 
-        <div>
-          <span>카테고리 : </span>
-          <span>{postInfo.category}</span>
+              <Title>{postInfo.title}</Title>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div>글 작성한 사람</div>
+                <WritingDate style={{ marginLeft: '40px' }}>
+                  작성일 {postInfo.createdDate}
+                </WritingDate>
+                <div style={{ marginLeft: 'auto' }}>
+                  <Link to={`/editpost/${postingId}`}>
+                    <button>수정하기</button>
+                  </Link>
+                  <button onClick={handleDeletePost}>삭제하기</button>
+                  <button>지원현황</button>
+                  <ApplyBtn postId={postingId}></ApplyBtn>
+                </div>
+              </div>
+              <Line />
+              <Body>{postInfo.body}</Body>
+              <div>
+                <span>카테고리 : </span>
+                <span>{postInfo.category}</span>
+              </div>
+              <div>
+                <span>기술스택 : </span>
+                <span>{postInfo.techs}</span>
+              </div>
+              <Line />
+            </PostDetailContainer>
+          </PostContatiner>
         </div>
-
-        <div>
-          <span>기술스택 : </span>
-          <span>{postInfo.techs}</span>
-        </div>
-        <Link to={`/editpost/${postingId}`}>
-          <button>수정하기</button>
-        </Link>
-        <button onClick={handleDeletePost}>삭제하기</button>
-        <ApplyBtn postId={postingId}></ApplyBtn>
-      </div>
+      </Background>
     </div>
   );
 }
 export default PostDetail;
+const PostContatiner = styled.div`
+  width: 1000px;
+  height: 100vh;
+  margin: 0 auto;
+  background-color: #fff;
+`;
+const PostDetailContainer = styled.div`
+  width: 850px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  position: flex;
+  position: relative;
+`;
+const Title = styled.div`
+  display: flex;
+  width: 815px;
+  height: 65.859px;
+  flex-direction: column;
+  justify-content: center;
+  flex-shrink: 0;
+
+  color: #000;
+  font-family: Big Shoulders Display;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+`;
+const Line = styled.div`
+  width: 850px;
+  height: 3px;
+  background: #e3e3e3;
+`;
+const Body = styled.div`
+  display: flex;
+  margin-top: 40px;
+  width: 850px;
+  height: 400px;
+
+  flex-shrink: 0;
+  color: #000;
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: 700;
+`;
